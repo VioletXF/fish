c73d9d9cbff189aef3667f91b54b201b792b73c9
VioletXF <zudevxf@gmail.com>
2022-08-02 12:32:52 +0900

    Add starship installation & Enable mouse scroll on LESS

--- config.fish
+++ config.fish
@@ -11,6 +11,21 @@ if status is-interactive
 	end
 	if type -q starship
 		starship init fish | source
+	else if not set -q DO_NOT_INSTALL_STARSHIP
+		read -l -P 'Install starship prompt? [Y/N] ' confirm
+		switch $confirm
+			case Y y
+				if type -q brew
+					brew install starship
+				else if type -q curl
+					curl -sS https://starship.rs/install.sh | sh
+					source $(status --current-filename)
+				else
+					echo 'Error: curl not found!' >&2
+				end
+			case N n ''
+				set -Ux DO_NOT_INSTALL_STARSHIP 1
+		end
 	end
 
 	test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
